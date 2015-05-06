c68f82e793bb24cf82ce834f078c1c447e1747d0
Evan Chen <echen@192.168.1.21>
2015-05-05 22:03:25 -0700

    First Push

---
+++ app/src/main/java/ech98/echen/stackable/BarcodeScannerActivity.java
@@ -0,0 +1,62 @@
+package ech98.echen.stackable;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.zbar.integration.android.ZBarConstants;
+import com.zbar.integration.android.ZBarScannerActivity;
+import net.sourceforge.zbar.Symbol;
+
+
+public class BarcodeScannerActivity extends ActionBarActivity {
+    private static final int ZBAR_SCANNER_REQUEST = 0;
+    private static final int ZBAR_QR_SCANNER_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        launchScanner();
+    }
+    public void launchScanner() {
+        if (isCameraAvailable()) {
+            Intent intent = new Intent(this, ZBarScannerActivity.class);
+            startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+        } else {
+            Toast.makeText(this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
+        }
+    }
+//    public void launchQRScanner(View v) {
+//        if (isCameraAvailable()) {
+//            Intent intent = new Intent(this, ZBarScannerActivity.class);
+//            intent.putExtra(ZBarConstants.SCAN_MODES, new int[]{Symbol.QRCODE});
+//            startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+//        } else {
+//            Toast.makeText(this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+    public boolean isCameraAvailable() {
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case ZBAR_SCANNER_REQUEST:
+            case ZBAR_QR_SCANNER_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(this, "Scan Result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_SHORT).show();
+                } else if(resultCode == RESULT_CANCELED && data != null) {
+                    String error = data.getStringExtra(ZBarConstants.ERROR_INFO);
+                    if(!TextUtils.isEmpty(error)) {
+                        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/ech98/echen/stackable/MainActivity.java
@@ -0,0 +1,34 @@
+package ech98.echen.stackable;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+    private TextView userSignupLink;
+    private Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userSignupLink = (TextView) findViewById(R.id.userSignupLink);
+        userSignupLink.setPaintFlags(userSignupLink.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main_AuthedActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
