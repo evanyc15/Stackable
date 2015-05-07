e72aee968894cccf323c37600d3a30c5783c0ecb
Evan Chen <echen@192.168.1.21>
2015-05-06 21:07:34 -0700

    Minor change

--- app/src/main/java/ech98/echen/stackable/MyListAdapter.java
+++ app/src/main/java/ech98/echen/stackable/MyListAdapter.java
@@ -1,8 +1,10 @@
 package ech98.echen.stackable;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.text.Layout;
 import android.view.LayoutInflater;
+import android.view.TouchDelegate;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -38,25 +40,64 @@ public class MyListAdapter extends ArrayAdapter<String> {
         View rowView = inflater.inflate(R.layout.my_list_row_item, parent, false);
         TextView textView = (TextView) rowView.findViewById(R.id.itemtext);
         textView.setText(data.get(position));
-        Button button = (Button) rowView.findViewById(R.id.delete_button);
-        button.setTag(position);
 
-        // Set click listener to remove item from ListView when imagebutton clicked
-        button.setOnClickListener(new View.OnClickListener() {
+        final View passrowView = rowView;
+        final int passPosition = position;
+
+        rowView.post(new Runnable() {
+            // Post in the parent's message queue to make sure the parent
+            // lays out its children before you call getHitRect()
             @Override
-            public void onClick(final View v) {
-                Animation.AnimationListener al = new Animation.AnimationListener() {
+            public void run() {
+                // The bounds for the delegate view (a Button
+                // in this example)
+                Rect delegateArea = new Rect();
+                Button button = (Button) passrowView.findViewById(R.id.delete_button);
+                button.setTag(passPosition);
+                button.setEnabled(true);
+                // Set click listener to remove item from ListView when imagebutton clicked
+                button.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onAnimationEnd(Animation arg0) {
-                        int tag = (Integer) v.getTag();
-                        data.remove(tag);
-                        notifyDataSetChanged();
-                    }
-                    @Override public void onAnimationRepeat(Animation animation) {}
-                    @Override public void onAnimationStart(Animation animation) {}
-                };
+                    public void onClick(final View v) {
+                        Animation.AnimationListener al = new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationEnd(Animation arg0) {
+                                int tag = (Integer) v.getTag();
+                                data.remove(tag);
+                                notifyDataSetChanged();
+                            }
 
-                collapse(v, al);
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {
+                            }
+
+                            @Override
+                            public void onAnimationStart(Animation animation) {
+                            }
+                        };
+
+                        collapse(v, al);
+                    }
+                });
+
+                // The hit rectangle for the Button
+                button.getHitRect(delegateArea);
+
+                // Extend the touch area of the Button beyond its bounds
+                delegateArea.right +=100;
+                delegateArea.left +=100;
+                delegateArea.top +=100;
+                delegateArea.bottom +=100;
+
+                // Instantiate a TouchDelegate.
+                // "delegateArea" is the bounds in local coordinates of
+                // the containing view to be mapped to the delegate view.
+                // "button" is the child view that should receive motion
+                // events.
+                TouchDelegate touchDelegate = new TouchDelegate(delegateArea, button);
+                if (View.class.isInstance(button.getParent())) {
+                    ((View) button.getParent()).setTouchDelegate(touchDelegate);
+                }
             }
         });
 
